071f2f22f9b08fd0fdc9c13d6790c628ad9ff64c
bearfish1ng <ju90e8@naver.com>
2024-10-09 19:33:38 +0900

    Update db_blog.sql

--- backend/model/db_blog.sql
+++ backend/model/db_blog.sql
@@ -7,5 +7,5 @@ CREATE TABLE blog_ (
     PRIMARY KEY(id)
     );
 
-INSERT INTO gallery_ (id, userid, title, img, content) VALUES
+INSERT INTO blog_ (id, userid, title, img, content) VALUES
 (1,'bearfish','디저트 종류','이미지','디저트를 먹으면');
