f6785302c558e9480d6d054313ed5c59ccf15e39
bearfish1ng <ju90e8@naver.com>
2024-10-09 17:00:08 +0900

    Add files via upload

---
+++ backend/model/db_blog.sql
@@ -0,0 +1,11 @@
+CREATE TABLE blog_ (
+    id int NOT NULL AUTO_INCREMENT,
+    userid varchar(25),
+    title varchar(25),
+    img varchar(400),
+    content varchar(100)
+    PRIMARY KEY(id)
+    );
+
+INSERT INTO gallery_ (id, userid, title, img, content) VALUES
+(1,'bearfish','디저트 종류','이미지','디저트를 먹으면')
